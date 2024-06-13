fca7bc5696b2f1daff6adf03bb4cf233f3d7b47d
parkhayarn <s0224y92@gmail.com>
2024-06-13 15:15:36 +0900

    빌드펑션 연습문제 풀이완료

--- 02_MYSQL_PRACTICE/15_BUILDINFUNCTION_PRACTICE.sql
+++ 02_MYSQL_PRACTICE/15_BUILDINFUNCTION_PRACTICE.sql
@@ -4,9 +4,32 @@ select
 from
     employee;
 -- EMPLOYEE 테이블에서 각 직원의 급여에서 10%를 감한 후, 이를 소수점 둘째 자리에서 반올림하여 출력하시오
+select
+    round(salary - (1 - 0.1))
+from
+    employee;
 
 -- EMPLOYEE 테이블에서 각 부서의 평균 급여를 계산하고, 그 값을 '부서별 평균 급여는 X 원입니다' 형식의 문자열로 출력하시오.
+select
+    concat('부서별 평균 급여는 ', avg(SALARY), '원 입니다')
+from
+    employee
+group by
+    DEPT_CODE;
 
 -- EMPLOYEE 테이블에서 각 직원의 입사 연도, 입사 월, 입사 일을 각각 조회하시오.
+select
+    date(HIRE_DATE)
+from
+    employee;
 
 -- EMPLOYEE 테이블에서 입사일이 2010년 1월 1일 이후인 직원들의 이름과 입사일을 조회하시오.
+select
+    EMP_NAME,
+    date(HIRE_DATE)
+from
+    employee
+where
+    date('2010-01-01') < date(HIRE_DATE)
+order by
+    date(HIRE_DATE);
